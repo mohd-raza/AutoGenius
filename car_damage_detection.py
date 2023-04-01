6e908dc1aba07b57f583beef5cf38c3f30784303
Yash Anil Joshi <80117906+joshiyash05@users.noreply.github.com>
2023-04-01 18:26:20 +0530

    Update car_damage_detection.py

--- car_damage_detection.py
+++ car_damage_detection.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
-from keras.preprocessing.image import load_img,img_to_array
+#from keras.preprocessing.image import load_img,img_to_array
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
 import numpy as np
 from keras.models import load_model
 import requests
